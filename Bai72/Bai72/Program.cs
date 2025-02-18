eb41d8581038b34b2cfaf108d5553d0a4e8abe7e
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-19 00:13:47 +0700

    init bai 72

---
+++ Bai72/Bai72/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai72
+{
+    //Bài 72: Tính S(x, n) = – x + x^2/2! – x^3/3! + … + (-1)^n * x^n/n!
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
