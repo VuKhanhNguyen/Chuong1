55bff54b11cfb41b040a59dae8cfdc39df0ee42e
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-18 16:10:41 +0700

    build code chua chay bai 12

--- Bai12/Bai12/Program.cs
+++ Bai12/Bai12/Program.cs
@@ -8,8 +8,32 @@ namespace Bai12
 {
     internal class Program
     {
+        static float TinhTongChuoi(float x_118 , int n_118)
+        {
+            int i_118=1;
+            float trc_118 = 1;
+            float s_118 = 0;
+            while (i_118 <= n_118)
+            {
+                trc_118 *= x_118;
+                s_118 += trc_118;
+                i_118++;
+            }
+            return s_118;
+           //Console.WriteLine("Tong la: {0}", s);
+        } 
+
         static void Main(string[] args)
         {
+            Console.Write("\nNhap x: ");
+            float x_118 = float.Parse(Console.ReadLine());
+
+            Console.Write("\nNhap n: ");
+            int n_118 = int.Parse(Console.ReadLine());
+
+            float total = TinhTongChuoi(x_118, n_118);
+
+            Console.WriteLine("Tong la: {0}", total);
         }
     }
 }
