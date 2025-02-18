7a1efad51397c2451d642b74359708d8456453e0
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-19 02:22:13 +0700

    them main

--- Bai82/Bai82/Program.cs
+++ Bai82/Bai82/Program.cs
@@ -13,7 +13,6 @@ namespace Bai82
         static void SoLonNhat(double a_118, double b_118, double c_118)
         {
             double max_118;
-
             if (a_118 > c_118)
             {
                 if (a_118 > b_118)
@@ -32,11 +31,21 @@ namespace Bai82
                 max_118 = c_118;
                 Console.Write("So lon nhat la: {0}", c_118);
             }
-            
+
+
         }
 
         static void Main(string[] args)
         {
+            int a_118, b_118, c_118;
+            Console.WriteLine("Nhap so thu 1: ");
+            a_118 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap so thu 2: ");
+            b_118 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap so thu 3: ");
+            c_118 = Convert.ToInt32(Console.ReadLine());
+            SoLonNhat(a_118, b_118, c_118);
+            
         }
     }
 }
