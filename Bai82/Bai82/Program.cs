be37b6eb3732936b04914db37cea6119df12afcb
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-19 02:10:58 +0700

    them method

--- Bai82/Bai82/Program.cs
+++ Bai82/Bai82/Program.cs
@@ -8,6 +8,33 @@ namespace Bai82
 {
     internal class Program
     {
+        //Bài 82: Viết chương trình tìm số lớn nhất trong 3 số thực a, b, c
+
+        static void SoLonNhat(double a_118, double b_118, double c_118)
+        {
+            double max_118;
+
+            if (a_118 > c_118)
+            {
+                if (a_118 > b_118)
+                {
+                    max_118 = a_118;
+                    Console.Write("So lon nhat la: {0}", a_118);
+                }
+                else
+                {
+                    max_118 = b_118;
+                    Console.Write("So lon nhat la: {0}", b_118);
+                }
+            }
+            else
+            {
+                max_118 = c_118;
+                Console.Write("So lon nhat la: {0}", c_118);
+            }
+            
+        }
+
         static void Main(string[] args)
         {
         }
