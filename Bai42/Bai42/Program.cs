cfe017ad4855683a0da49d3e9f3be8217b455927
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-18 22:34:57 +0700

    them de bai tap va sua anh chup code voi ket qua console

--- Bai42/Bai42/Program.cs
+++ Bai42/Bai42/Program.cs
@@ -8,7 +8,8 @@ namespace Bai42
 {
     internal class Program
     {
-
+        //Bài 42: Cho n là số nguyên dương. Hãy tìm giá trị nguyên dương k lớn nhất sao cho S(k)  < n.
+        //Trong đó chuỗi k được định nghĩa như sau: S(k) = 1 + 2 + 3 + … + k
         static void MaxofNguyenDuong(int n_118)
         {
             int k_118 = 0;
