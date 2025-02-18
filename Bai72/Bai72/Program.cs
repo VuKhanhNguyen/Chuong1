eaaa1cbddb6f911773ca6fad64447b47377ef16b
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-19 00:43:00 +0700

    them method giaithua

--- Bai72/Bai72/Program.cs
+++ Bai72/Bai72/Program.cs
@@ -9,8 +9,24 @@ namespace Bai72
     //Bài 72: Tính S(x, n) = – x + x^2/2! – x^3/3! + … + (-1)^n * x^n/n!
     internal class Program
     {
+
+        static int giaithua(int n_118)
+        {
+            if (n_118==0)
+            {
+                return 1;
+            }
+            else
+            {
+               return n_118 * giaithua(n_118-1);
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
+
         }
     }
 }
