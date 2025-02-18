5e0635dd669af13228015ab583343022d78007d9
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-19 03:07:24 +0700

    them method

--- Bai92/Bai92/Program.cs
+++ Bai92/Bai92/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,20 @@ namespace Bai92
 {
     internal class Program
     {
+
+        static int UCLN(int a_118, int b_118)
+        {
+            if(a_118 == b_118)
+                return a_118;
+            while (b_118 != 0)
+            {
+                int r = a_118 % b_118;
+                a_118 = b_118;
+                b_118 = r;
+            }
+            return a_118;
+        }
+        
         static void Main(string[] args)
         {
         }
