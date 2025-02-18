86865f21c7f5b5de9f96abbf65a294108d844618
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-18 23:48:11 +0700

    them method

--- Bai62/Bai62/Program.cs
+++ Bai62/Bai62/Program.cs
@@ -8,8 +8,31 @@ namespace Bai62
 {
     internal class Program
     {
+        //Bài 62: Cho 2 số nguyên dương a và b.Hãy tìm ước chung lớn nhất của 2 số này.
+        static int ucln(int a_118, int b_118)
+        {
+            if (a_118 == 0 || b_118 == 0)
+            {
+                return a_118 + b_118;
+
+            }
+            while (a_118 != b_118)
+            {
+                if (a_118 > b_118)
+                {
+                    a_118 = a_118 - b_118;
+                }
+                else
+                {
+                    b_118 = b_118 - a_118;
+                }
+            }
+            return a_118;
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
